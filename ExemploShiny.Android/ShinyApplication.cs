f394379e6ef6cca1c0b26f0511559ba9a8f5e8e9
Thiago Bertuzzi <thiago.bertuzzi@gmail.com>
2019-11-22 10:27:55 -0300

    :tada: Subindo Exemplo no github

---
+++ ExemploShiny.Android/ShinyApplication.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Android.App;
+using Android.Runtime;
+
+namespace ExemploShiny.Droid
+{
+    [Application]
+    public class ShinyApplication : Shiny.ShinyAndroidApplication<Startup>
+    {
+        public ShinyApplication(IntPtr handle, JniHandleOwnership transfer) : base(handle, transfer)
+        {
+
+        }
+    }
+}
---
+++ ExemploShiny/Startup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Shiny;
+
+namespace ExemploShiny
+{
+    public class Startup : ShinyStartup
+    {
+        public override void ConfigureServices(IServiceCollection services)
+        {
+            
+        }
+    }
+}
